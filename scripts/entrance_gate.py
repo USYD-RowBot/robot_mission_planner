48c31fa9b18b6f74b83ba08dae77583f9db4ce5a
acenturyandabit <steeven.liu2@gmail.com>
2018-07-26 13:50:57 +1000

    fixed indentation issues in smacher

--- scripts/entrance_gate.py
+++ scripts/entrance_gate.py
@@ -5,16 +5,147 @@ import smach_ros
 
 class waitForStart(smach.State):
    def __init__(self):
-	 smach.State.__init__(self, outcomes=['abort'])
+     smach.State.__init__(self, outcomes=['abort', 'start'])
    def execute(self,userdata):
-	     rospy.loginfo('Waiting for start...')
-	     rospy.sleep(2)
-	    if True:
-	    	rospy.loginfo('Start aborted by controller.')
-	    	return 'abort'
+         rospy.loginfo('Waiting for start...')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('Start aborted by controller.')
+             return 'abort'
+         if True:
+             rospy.loginfo('Start signal recieved.')
+             return 'start'
+
+class findGates(smach.State):
+   def __init__(self):
+     smach.State.__init__(self, outcomes=['gatesFound', 'gateTimeout', 'criticalFail'])
+   def execute(self,userdata):
+         rospy.loginfo('Finding Gates...')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('Found Gates!')
+             return 'gatesFound'
+         if True:
+             rospy.loginfo('Not all gates could be found in allocated time period. Proceed with caution!')
+             return 'gateTimeout'
+         if True:
+             rospy.loginfo('No gates could be found. Critical error! Aborting...')
+             return 'criticalFail'
+
+class chooseEntry(smach.State):
+   def __init__(self):
+     smach.State.__init__(self, outcomes=['entryFound', 'entryRandomGuess', 'criticalFail'])
+   def execute(self,userdata):
+         rospy.loginfo('Choosing entry gate...')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('Chosen entry gate!')
+             return 'entryFound'
+         if True:
+             rospy.loginfo('Not sure which gate to go through; making random guess :/')
+             return 'entryRandomGuess'
+         if True:
+             rospy.loginfo('Not sure which gate to go through: aborting - better safe than sorry!')
+             return 'criticalFail'
+
+class navEntry(smach.State):
+   def __init__(self):
+     smach.State.__init__(self, outcomes=['gatePassthroughOK', 'minorHindrance', 'criticalFail'])
+   def execute(self,userdata):
+         rospy.loginfo('Navigating to entry...')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('Passed through entry gate!')
+             return 'gatePassthroughOK'
+         if True:
+             rospy.loginfo('Something went wrong, but we are still alive... Moving on.')
+             return 'minorHindrance'
+         if True:
+             rospy.loginfo('Something went pretty badly wrong. hlep pls')
+             return 'criticalFail'
+
+class findBuoys(smach.State):
+   def __init__(self):
+     smach.State.__init__(self, outcomes=['2buoysFound', '1buoyFound', 'criticalFail'])
+   def execute(self,userdata):
+         rospy.loginfo('Identifying buoys...')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('Got both buoys.')
+             return '2buoysFound'
+         if True:
+             rospy.loginfo('Got one buoy. Lets just use that one :3')
+             return '1buoyFound'
+         if True:
+             rospy.loginfo('No buoys could be found. what do .-.')
+             return 'criticalFail'
+
+class chooseBuoy(smach.State):
+   def __init__(self):
+     smach.State.__init__(self, outcomes=['buoyFoundConfident', 'buoyNoLock', 'criticalFail'])
+   def execute(self,userdata):
+         rospy.loginfo('Choosing buoy...')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('Got a confident lock on the appropriate buoy.')
+             return 'buoyFoundConfident'
+         if True:
+             rospy.loginfo('Not sure which buoy to go around. Just choosing a random one')
+             return 'buoyNoLock'
+         if True:
+             rospy.loginfo('Aliens must have abducted the beacons or sth 3: what do .-.')
+             return 'criticalFail'
+
+class navCircleBuoy(smach.State):
+   def __init__(self):
+     smach.State.__init__(self, outcomes=['orbitDone', 'whereRWe', 'criticalFail'])
+   def execute(self,userdata):
+         rospy.loginfo('Orbiting buoys.')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('Orbit done.')
+             return 'orbitDone'
+         if True:
+             rospy.loginfo('wh-- what happened? ah well im alive lets keep going')
+             return 'whereRWe'
+         if True:
+             rospy.loginfo('X-X')
+             return 'criticalFail'
+
+class chooseExit(smach.State):
+   def __init__(self):
+     smach.State.__init__(self, outcomes=['choiceOK', 'choiceUnsure', 'criticalFail'])
+   def execute(self,userdata):
+         rospy.loginfo('Choosing exit gate...')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('Chose the exit buoy. Allg.')
+             return 'choiceOK'
+         if True:
+             rospy.loginfo('Not sure which buoy to go through. Choosing a random one...')
+             return 'choiceUnsure'
+         if True:
+             rospy.loginfo('No buoys could be found. what do .-.')
+             return 'criticalFail'
+
+class navigateThroughExit(smach.State):
+   def __init__(self):
+     smach.State.__init__(self, outcomes=['navOK', 'navError', 'criticalFail'])
+   def execute(self,userdata):
+         rospy.loginfo('Heading through exit...')
+         rospy.sleep(2)
+         if True:
+             rospy.loginfo('All done!')
+             return 'navOK'
+         if True:
+             rospy.loginfo('Something went wrong but i think i am ok...')
+             return 'navError'
+         if True:
+             rospy.loginfo('Nooo! So close!')
+             return 'criticalFail'
 
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     rospy.init_node(entrance_gate)
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['success', 'fail'])
@@ -22,6 +153,22 @@ class waitForStart(smach.State):
     with sm:
     # Add states to the container
         smach.StateMachine.add('waitForStart', waitForStart(), 
-                              transitions={'abort': 'fail'})
+                              transitions={'abort': 'fail', 'start': 'findGates'})
+        smach.StateMachine.add('findGates', findGates(), 
+                              transitions={'gatesFound': 'chooseEntry', 'gateTimeout': 'findGates', 'criticalFail': 'fail'})
+        smach.StateMachine.add('chooseEntry', chooseEntry(), 
+                              transitions={'entryFound': 'navEntry', 'entryRandomGuess': 'navEntry', 'criticalFail': 'fail'})
+        smach.StateMachine.add('navEntry', navEntry(), 
+                              transitions={'gatePassthroughOK': 'findBuoys', 'minorHindrance': 'findBuoys', 'criticalFail': 'fail'})
+        smach.StateMachine.add('findBuoys', findBuoys(), 
+                              transitions={'2buoysFound': 'chooseBuoy', '1buoyFound': 'chooseBuoy', 'criticalFail': 'fail'})
+        smach.StateMachine.add('chooseBuoy', chooseBuoy(), 
+                              transitions={'buoyFoundConfident': 'navCircleBuoy', 'buoyNoLock': 'navCircleBuoy', 'criticalFail': 'fail'})
+        smach.StateMachine.add('navCircleBuoy', navCircleBuoy(), 
+                              transitions={'orbitDone': 'chooseExit', 'whereRWe': 'chooseExit', 'criticalFail': 'fail'})
+        smach.StateMachine.add('chooseExit', chooseExit(), 
+                              transitions={'choiceOK': 'choiceOK', 'choiceUnsure': 'chooseBuoy', 'criticalFail': 'fail'})
+        smach.StateMachine.add('navigateThroughExit', navigateThroughExit(), 
+                              transitions={'navOK': 'success', 'navError': 'success', 'criticalFail': 'fail'})
     # Execute SMACH plan
     outcome = sm.execute()
--- scripts/smacher.py
+++ scripts/smacher.py
@@ -1,16 +1,73 @@
 stateMachineName="entrance_gate"
 finalOutcomes=['success','fail']
 states=[
-#	{'name':'name',
+#   {'name':'name',
 #    'mymessage':'Something to say at the start',
-#	 'transitions':[
-#	 	('pathName','toStateName','Relevant output message')
-#	 ]},
-	{'name':'waitForStart',
+#    'transitions':[
+#       ('pathName','toStateName','Relevant output message')
+#     ]},
+    {'name':'waitForStart',
     'mymessage':'Waiting for start...',
     'transitions':[
-	 	('abort','fail','Start aborted by controller.')
-	 ]}
+         ('abort','Start aborted by controller.','fail'),
+        ('start','Start signal recieved.','findGates'),
+     ]},
+     {'name':'findGates',
+    'mymessage':'Finding Gates...',
+    'transitions':[
+         ('gatesFound','Found Gates!','chooseEntry'),
+        ('gateTimeout','Not all gates could be found in allocated time period. Proceed with caution!','findGates'),
+        ('criticalFail','No gates could be found. Critical error! Aborting...','fail')
+     ]},
+     {'name':'chooseEntry',
+    'mymessage':'Choosing entry gate...',
+    'transitions':[
+         ('entryFound','Chosen entry gate!','navEntry'),
+        ('entryRandomGuess','Not sure which gate to go through; making random guess :/','navEntry'),
+        ('criticalFail','Not sure which gate to go through: aborting - better safe than sorry!','fail')
+     ]},
+     {'name':'navEntry',
+    'mymessage':'Navigating to entry...',
+    'transitions':[
+         ('gatePassthroughOK','Passed through entry gate!','findBuoys'),
+        ('minorHindrance','Something went wrong, but we are still alive... Moving on.','findBuoys'),
+        ('criticalFail','Something went pretty badly wrong. hlep pls','fail')
+     ]},
+     {'name':'findBuoys',
+    'mymessage':'Identifying buoys...',
+    'transitions':[
+         ('2buoysFound','Got both buoys.','chooseBuoy'),
+        ('1buoyFound','Got one buoy. Lets just use that one :3','chooseBuoy'),
+        ('criticalFail','No buoys could be found. what do .-.','fail')
+     ]},
+     {'name':'chooseBuoy',
+    'mymessage':'Choosing buoy...',
+    'transitions':[
+         ('buoyFoundConfident','Got a confident lock on the appropriate buoy.','navCircleBuoy'),
+        ('buoyNoLock','Not sure which buoy to go around. Just choosing a random one','navCircleBuoy'),
+        ('criticalFail','Aliens must have abducted the beacons or sth 3: what do .-.','fail')
+     ]},
+     {'name':'navCircleBuoy',
+    'mymessage':'Orbiting buoys.',
+    'transitions':[
+         ('orbitDone','Orbit done.','chooseExit'),
+        ('whereRWe','wh-- what happened? ah well im alive lets keep going','chooseExit'),
+        ('criticalFail','X-X','fail')
+     ]},
+     {'name':'chooseExit',
+    'mymessage':'Choosing exit gate...',
+    'transitions':[
+         ('choiceOK','Chose the exit buoy. Allg.','choiceOK'),
+        ('choiceUnsure','Not sure which buoy to go through. Choosing a random one...','chooseBuoy'),
+        ('criticalFail','No buoys could be found. what do .-.','fail')
+     ]},
+     {'name':'navigateThroughExit',
+    'mymessage':'Heading through exit...',
+    'transitions':[
+         ('navOK','All done!','success'),
+        ('navError','Something went wrong but i think i am ok...','success'),
+        ('criticalFail','Nooo! So close!','fail')
+     ]},
 ]
 
 
@@ -25,16 +82,16 @@ with open(stateMachineName+".py",'w+') as f:
         transitNames=[it[0] for it in i['transitions']]
         stateString+="\nclass "+i['name']+"(smach.State):\n"
         stateString+="   def __init__(self):\n"
-        stateString+="	 smach.State.__init__(self, outcomes={0})\n".format(transitNames)
+        stateString+="     smach.State.__init__(self, outcomes={0})\n".format(transitNames)
         stateString+="   def execute(self,userdata):\n"
-        stateString+="	     rospy.loginfo('{0}')\n".format(i['mymessage'])
-        stateString+="	     rospy.sleep(2)\n";
+        stateString+="         rospy.loginfo('{0}')\n".format(i['mymessage'])
+        stateString+="         rospy.sleep(2)\n";
         for c,j in enumerate(i['transitions']):
-            stateString+="	    if True:\n"
-            stateString+="	    	rospy.loginfo('{0}')\n".format(j[2])
-            stateString+="	    	return '{0}'\n".format(j[0]);
+            stateString+="         if True:\n"
+            stateString+="             rospy.loginfo('{0}')\n".format(j[1])
+            stateString+="             return '{0}'\n".format(j[0]);
     f.write(stateString);
-    f.write("\n\n if __name__ == '__main__':\n"
+    f.write("\n\nif __name__ == '__main__':\n"
     "    rospy.init_node({0})\n".format(stateMachineName)+
     "    # Create a SMACH state machine\n"
     "    sm = smach.StateMachine(outcomes={0})\n".format(finalOutcomes)+
@@ -43,7 +100,7 @@ with open(stateMachineName+".py",'w+') as f:
     "    # Add states to the container\n");
     stateString="";
     for i in states:
-        transitDict=[(j[0],j[1]) for j in i['transitions']]
+        transitDict=[(j[0],j[2]) for j in i['transitions']]
         transitDict=dict(transitDict)
         stateString+="        smach.StateMachine.add('{0}', ".format(i['name'])+i['name']+"(), \n"
         stateString+="                              transitions={0})\n".format(transitDict);
