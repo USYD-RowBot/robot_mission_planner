acd17738d2b28e37801be3b39533120b393b480a
acenturyandabit <steeven.liu2@gmail.com>
2018-12-13 13:31:32 -1000

    refactor for easier subsmaching

--- scripts/demonstrate_navigation.py
+++ scripts/demonstrate_navigation.py
@@ -15,120 +15,117 @@ from smach_ros import SimpleActionState
 from geometry_msgs.msg import PoseStamped
 import tf
 
+machine=smach.StateMachine(outcomes=['success', 'fail'])
+
+with machine:
+    # Add states to the container
+    # waitForStart state
+    #smach.StateMachine.add('waitForStart', waitForStart(), transitions={
+    #                       'started': 'findGates', 'aborted': 'fail'})
+
+    # Gate finding
+    smach.StateMachine.add('findGates',
+                            searchFor(
+                                targets=['red buoy', 'red buoy', 'green buoy', 'green buoy']),
+                            transitions={'found': 'find_start', 'partial_found': 'fail', 'timeout': 'fail'},
+                            remapping={"objects":"data"})
+    def midpointStart(data):
+        # Extract IDs of start and end beacons
+        items = []
+        listener = tf.TransformListener()
+        for i in data:
+            # im assuming this is the relevant thing?
+            listener.waitForTransform(
+                '/'+i.frame_id, '/base_link', rospy.Time(0), rospy.Duration(4.0))
+            (trans, rot) = listener.lookupTransform(
+                '/'+i.frame_id, '/base_link', rospy.Time(0))
+            dist = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+            items.append((dist, i.best_guess, i.frame_id))
+        # sort to give us closest items.
+        items = sorted(items, key=lambda i: i[0])
+        firstRed = None
+        firstGreen = None
+        for i in items:
+            if firstRed is None and i[1].find("red"):
+                firstRed = i
+            if firstGreen is None and i[1].find("green"):
+                firstGreen = i
+        (rtrans, rot) = listener.lookupTransform(
+            firstRed[2], '/map', rospy.Time(0))
+        (gtrans, rot) = listener.lookupTransform(
+            firstGreen[2], '/map', rospy.Time(0))
+        midpoint = (-(rtrans[0]+gtrans[0])/2, -(rtrans[1]+gtrans[1])/2)
+        angle = math.atan2((rtrans[0]-gtrans[0]),
+                            (rtrans[1]-gtrans[1]))+math.pi/4
+        mba = PoseStamped()
+        mba.pose.position.x = midpoint[0]
+        mba.pose.position.y = midpoint[1]
+        mba.pose.position.z = 0.0
+        # fix quarternion
+        mba.pose.orientation.x = 0
+        mba.pose.orientation.y = 0
+        mba.pose.orientation.z = 0
+        mba.pose.orientation.w = angle
+        print(mba)
+        print(rtrans)
+        print(gtrans)
+        return mba  # my code has a degree yay
+    smach.StateMachine.add('find_start', executor(midpointStart),transitions={'done': 'navigate_to_start', 'error': 'fail'},remapping={"result":"goal"})
+    smach.StateMachine.add('navigate_to_start', moveTo(),transitions={'succeeded': 'find_end', 'preempted': 'fail', 'aborted': 'fail'})
+
+    def midpointEnd(data):
+        # Extract IDs of start and end beacons
+        items = []
+        listener = tf.TransformListener()
+        for i in data:
+            # im assuming this is the relevant thing?
+            listener.waitForTransform(
+                '/'+i.frame_id, '/map', rospy.Time(0), rospy.Duration(4.0))
+            (trans, rot) = listener.lookupTransform(
+                '/'+i.frame_id, '/map', rospy.Time(0))
+            dist = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+            items.append((dist, i.best_guess, i.frame_id))
+        # sort to give us closest items.
+        items = sorted(items, key=lambda i: i[0],reverse=True)
+        firstRed = None
+        firstGreen = None
+        for i in items:
+            if firstRed is None and i[1].find("red"):
+                firstRed = i
+            if firstGreen is None and i[1].find("green"):
+                firstGreen = i
+        (rtrans, rot) = listener.lookupTransform(
+            firstRed[2], '/map', rospy.Time(0))
+        (gtrans, rot) = listener.lookupTransform(
+            firstGreen[2], '/map', rospy.Time(0))
+        midpoint = (-(rtrans[0]+gtrans[0])/2, -(rtrans[1]+gtrans[1])/2)
+        angle = math.atan2((rtrans[0]-gtrans[0]),
+                            (rtrans[1]-gtrans[1]))+math.pi/4
+        mba = PoseStamped()
+        mba.pose.position.x = midpoint[0]
+        mba.pose.position.y = midpoint[1]
+        mba.pose.position.z = 0.0
+        # fix quarternion
+        mba.pose.orientation.x = 0
+        mba.pose.orientation.y = 0
+        mba.pose.orientation.z = 0
+        mba.pose.orientation.w = angle
+        print(mba)
+        print(rtrans)
+        print(gtrans)
+        return mba  # my code has a degree yay
+    smach.StateMachine.add('find_end', executor(midpointEnd),transitions={'done': 'navigate_to_end', 'error': 'fail'},remapping={"result":"goal"})
+    smach.StateMachine.add('navigate_to_end', moveTo(),transitions={'succeeded': 'success', 'preempted': 'fail', 'aborted': 'fail'})
+
 if __name__ == '__main__':
     rospy.init_node('smach_demonav_state_machine')
-    # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['success', 'fail'])
-    # Open the container
-    with sm:
-        # Add states to the container
-        # waitForStart state
-        #smach.StateMachine.add('waitForStart', waitForStart(), transitions={
-        #                       'started': 'findGates', 'aborted': 'fail'})
-
-        # Gate finding
-        smach.StateMachine.add('findGates',
-                               searchFor(
-                                   targets=['red buoy', 'red buoy', 'green buoy', 'green buoy']),
-                               transitions={'found': 'find_start', 'partial_found': 'fail', 'timeout': 'fail'},
-                               remapping={"objects":"data"})
-
-        def midpointStart(data):
-            # Extract IDs of start and end beacons
-            items = []
-            listener = tf.TransformListener()
-            for i in data:
-                # im assuming this is the relevant thing?
-                listener.waitForTransform(
-                    '/'+i.frame_id, '/base_link', rospy.Time(0), rospy.Duration(4.0))
-                (trans, rot) = listener.lookupTransform(
-                    '/'+i.frame_id, '/base_link', rospy.Time(0))
-                dist = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-                items.append((dist, i.best_guess, i.frame_id))
-            # sort to give us closest items.
-            items = sorted(items, key=lambda i: i[0])
-            firstRed = None
-            firstGreen = None
-            for i in items:
-                if firstRed is None and i[1].find("red"):
-                    firstRed = i
-                if firstGreen is None and i[1].find("green"):
-                    firstGreen = i
-            (rtrans, rot) = listener.lookupTransform(
-                firstRed[2], '/map', rospy.Time(0))
-            (gtrans, rot) = listener.lookupTransform(
-                firstGreen[2], '/map', rospy.Time(0))
-            midpoint = (-(rtrans[0]+gtrans[0])/2, -(rtrans[1]+gtrans[1])/2)
-            angle = math.atan2((rtrans[0]-gtrans[0]),
-                               (rtrans[1]-gtrans[1]))+math.pi/4
-            mba = PoseStamped()
-            mba.pose.position.x = midpoint[0]
-            mba.pose.position.y = midpoint[1]
-            mba.pose.position.z = 0.0
-            # fix quarternion
-            mba.pose.orientation.x = 0
-            mba.pose.orientation.y = 0
-            mba.pose.orientation.z = 0
-            mba.pose.orientation.w = angle
-            print(mba)
-            print(rtrans)
-            print(gtrans)
-            return mba  # my code has a degree yay
-        smach.StateMachine.add('find_start', executor(midpointStart),transitions={'done': 'navigate_to_start', 'error': 'fail'},remapping={"result":"goal"})
-        smach.StateMachine.add('navigate_to_start', moveTo(),transitions={'succeeded': 'find_end', 'preempted': 'fail', 'aborted': 'fail'})
-
-        def midpointEnd(data):
-            # Extract IDs of start and end beacons
-            items = []
-            listener = tf.TransformListener()
-            for i in data:
-                # im assuming this is the relevant thing?
-                listener.waitForTransform(
-                    '/'+i.frame_id, '/map', rospy.Time(0), rospy.Duration(4.0))
-                (trans, rot) = listener.lookupTransform(
-                    '/'+i.frame_id, '/map', rospy.Time(0))
-                dist = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-                items.append((dist, i.best_guess, i.frame_id))
-            # sort to give us closest items.
-            items = sorted(items, key=lambda i: i[0],reverse=True)
-            firstRed = None
-            firstGreen = None
-            for i in items:
-                if firstRed is None and i[1].find("red"):
-                    firstRed = i
-                if firstGreen is None and i[1].find("green"):
-                    firstGreen = i
-            (rtrans, rot) = listener.lookupTransform(
-                firstRed[2], '/map', rospy.Time(0))
-            (gtrans, rot) = listener.lookupTransform(
-                firstGreen[2], '/map', rospy.Time(0))
-            midpoint = (-(rtrans[0]+gtrans[0])/2, -(rtrans[1]+gtrans[1])/2)
-            angle = math.atan2((rtrans[0]-gtrans[0]),
-                               (rtrans[1]-gtrans[1]))+math.pi/4
-            mba = PoseStamped()
-            mba.pose.position.x = midpoint[0]
-            mba.pose.position.y = midpoint[1]
-            mba.pose.position.z = 0.0
-            # fix quarternion
-            mba.pose.orientation.x = 0
-            mba.pose.orientation.y = 0
-            mba.pose.orientation.z = 0
-            mba.pose.orientation.w = angle
-            print(mba)
-            print(rtrans)
-            print(gtrans)
-            return mba  # my code has a degree yay
-        smach.StateMachine.add('find_end', executor(midpointEnd),transitions={'done': 'navigate_to_end', 'error': 'fail'},remapping={"result":"goal"})
-        smach.StateMachine.add('navigate_to_end', moveTo(),transitions={'succeeded': 'success', 'preempted': 'fail', 'aborted': 'fail'})
-
-    # Execute SMACH plan
+    # start introspection server
     # Create and start the introspection server
-    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
-    sis.start()
-
+    if rospy.get_param("machineDebug",False)==True:
+        sis = smach_ros.IntrospectionServer('server_name', machine, '/SM_ROOT')
+        sis.start()
     # Execute the state machine
-    outcome = sm.execute()
-
+    outcome = machine.execute()
     # Wait for ctrl-c to stop the application
     rospy.spin()
     sis.stop()
--- scripts/entrance_gate.py
+++ scripts/entrance_gate.py
@@ -3,99 +3,169 @@
 import rospy
 import smach
 import smach_ros
-from action_servers.searchFor import searchFor
-from action_servers.moveToPos import moveToPos
-from smach_ros import SimpleActionState
-from action_servers.waitForStart import waitForStart
-from rowbot_mission_planner.msg import searchForAction, searchForGoal
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import tf
+import math
+from baseFuncs.searchFor import searchFor
+from baseFuncs.moveTo import moveTo
 
 
-class chooseEntry(smach.State):
-   def __init__(self):
-     smach.State.__init__(self, outcomes=['entryFound', 'entryRandomGuess', 'criticalFail'],output_keys=['chooseEntry_tfref'])
-     self.times=0;
-   def execute(self,userdata):
-         rospy.loginfo('Choosing entry gate using sonar data.')
-         rospy.sleep(2)
-         userdata.chooseEntry_tfref="tf_reference_string"; ##  to edit - get from actual sensor data
-         if self.times==0:
-             rospy.loginfo('Chosen entry gate!')
-             self.times++;
-             return 'entryFound'
-         elif self.times==1:
-             rospy.loginfo('Chosen exit gate!')
-             return 'exitFound'
-         if True:
-             rospy.loginfo('Not sure which gate to go through: aborting - better safe than sorry!')
-             return 'criticalFail'
+machine = smach.StateMachine(outcomes=['success', 'fail'])
 
 
-class chooseBuoy(smach.State):
-   def __init__(self):
-     smach.State.__init__(self, outcomes=['buoyFoundConfident', 'buoyNoLock', 'criticalFail'],output_keys=['chooseBuoy_tfref'])
-   def execute(self,userdata):
-         rospy.loginfo('Choosing buoy...')
-         rospy.sleep(2)
-         if True:
-             userdata.chooseBuoy_tfref="tf_reference_string"; ##  to edit - get from actual sensor data
-             rospy.loginfo('Got a confident lock on the appropriate buoy.')
-             return 'buoyFoundConfident'
-         if True:
-             rospy.loginfo('Not sure which buoy to go around. Just choosing a random one')
-             return 'buoyNoLock'
-         if True:
-             rospy.loginfo('Aliens must have abducted the beacons or sth 3: what do .-.')
-             return 'criticalFail'
+def isCollinear(p1, p2, p3):
+     a = p1[0] * (p2[1] - p3[1]) + p2[0] * \
+                  (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1])
+    if (a < 10): 
+        return True
+    else: 
+        return False
 
-thisName='entrance_gate';
-if __name__ == '__main__':
-    rospy.init_node(thisName)
-    # Start required actionservers
-    searchForServer=searchFor();
-    
-    
-    # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['success', 'fail'])
-    # Open the container
-    with sm:
+with machine:
     # Add states to the container
-        smach.StateMachine.add('waitForStart',
-                              smach_ros.SimpleActionState('waitForStart', waitForStartAction,
-                                                       goal = waitForStartGoal(smachName=thisName)),
-                              transitions={'succeeded':'chooseEntry'})
-        smach.StateMachine.add('findGates',
-                              smach_ros.SimpleActionState('searchFor', searchForAction, goal = searchForGoal(target=['gates'])), ### Or something.
-                              transitions={'succeeded':'moveToStart'})
-        smach.StateMachine.add('moveToStart',
-                              smach_ros.SimpleActionState('move_base', MoveBaseAction, goal_cb=startMidpoint),
-                              transitions={'succeeded':'moveToEnd'}
-        )
-        smach.StateMachine.add('moveToEnd',
-                              smach_ros.SimpleActionState('moveToPos', moveToPosAction,
-                                                       goal_slots = ['tfName']), ### Or something.
-                              transitions={'succeeded':'findBuoys'},
-                              remapping={'tfName':'chooseEntry_tfref'})
-        smach.StateMachine.add('findBuoys',
-                              smach_ros.SimpleActionState('searchFor', searchForAction,
-                                                            goal = searchForGoal(target=['lightBuoy','markerBuoy'])), ### Or something.
-                              transitions={'succeeded':'chooseBuoy'}))
-        smach.StateMachine.add('chooseBuoy', chooseBuoy(), 
-                              transitions={'buoyFoundConfident': 'navCircleBuoy', 'buoyNoLock': 'navCircleBuoy', 'criticalFail': 'fail'})
-        def navCircleBuoy_goal_cb(userdata, goal):
-            orbiter_goal = orbitPosGoal(radius=20,spinRadians=3.141); # should probably pick midpoint distance between buoys instead of a constant.
-            orbiter_goal.tfName = userdata.chooseBuoy_tfref
-            return gripper_goal
-        smach.StateMachine.add('navCircleBuoy',
-                              smach_ros.SimpleActionState('orbitPos', orbitPosAction,
-                                                       goal_cb=navCircleBuoy_goal_cb,
-                                                       input_keys=['chooseBuoy_tfref']),
-                              transitions={'succeeded':'findGates'}, ## go back to find gates again.
-                              )
-        smach.StateMachine.add('navExit',
-                              smach_ros.SimpleActionState('moveToPos', moveToPosAction,
-                                                       goal_slots = ['tfName']),
-                              transitions={'succeeded':'findBuoys'},
-                              remapping={'tfName':'chooseEntry_tfref'})
-    # Execute SMACH plan
-    outcome = sm.execute()
+
+    # Gate finding
+    smach.StateMachine.add('findGates',
+                            searchFor(
+                                targets=['red buoy', 'green buoy', 'white buoy']),
+                            transitions={'found': 'findGates', 'partial_found': 'fail', 'timeout': 'fail'},
+                            remapping={"objects":"data"})
+    
+    def find_beacons(data):
+        # Extract IDs of start and end beacons
+        items = []
+        listener = tf.TransformListener()
+        for i in data:
+            # im assuming this is the relevant thing?
+            (trans, rot) = listener.lookupTransform(
+                '/'+i.frame_id, '/base_link', rospy.Time(0))
+            dist = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+            items.append((trans, i.best_guess, i.frame_id, dist))
+        # figre out where the 4 entrance gates are: 4 buoys in line. 
+        gates=False
+        for i in items:
+         for j in items:
+          if i != j:
+           for k in items:
+            if k!=i and k!=j:
+             if isCollinear(i[0],j[0],k[0]):
+              for l in items:
+               if l!=i and l!=j and l!=k:
+                if isCollinear(i[0],j[0],l[0]):
+                 gates=[i,j,k,l]
+                 # yay we found it after 23i423 nestings.
+        # Order the gates by their position along the line (just one of the x or y axes will do... take the larger one!)
+        xory, value = max(enumerate([gates[0][0][0]-gates[1][0][0],gates[0][0][1]-gates[1][0][1]]), key=lambda i: i[1])
+        items = sorted(gates, key=lambda i: i[0][xory])
+        # stop here and return
+        return items
+
+    def chooseGate(data):
+        # Extract IDs of start and end beacons
+        items = []
+        listener = tf.TransformListener()
+        for i in data:
+            # im assuming this is the relevant thing?
+            listener.waitForTransform(
+                '/'+i.frame_id, '/base_link', rospy.Time(0), rospy.Duration(4.0))
+            (trans, rot) = listener.lookupTransform(
+                '/'+i.frame_id, '/base_link', rospy.Time(0))
+            dist = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+            items.append((dist, i.best_guess, i.frame_id))
+        # sort to give us closest items.
+        items = sorted(items, key=lambda i: i[0])
+        firstRed = None
+        firstGreen = None
+        for i in items:
+            if firstRed is None and i[1].find("red"):
+                firstRed = i
+            if firstGreen is None and i[1].find("green"):
+                firstGreen = i
+        (rtrans, rot) = listener.lookupTransform(
+            firstRed[2], '/map', rospy.Time(0))
+        (gtrans, rot) = listener.lookupTransform(
+            firstGreen[2], '/map', rospy.Time(0))
+        midpoint = (-(rtrans[0]+gtrans[0])/2, -(rtrans[1]+gtrans[1])/2)
+        angle = math.atan2((rtrans[0]-gtrans[0]),
+                            (rtrans[1]-gtrans[1]))+math.pi/4
+        mba = PoseStamped()
+        mba.pose.position.x = midpoint[0]
+        mba.pose.position.y = midpoint[1]
+        mba.pose.position.z = 0.0
+        # fix quarternion
+        mba.pose.orientation.x = 0
+        mba.pose.orientation.y = 0
+        mba.pose.orientation.z = 0
+        mba.pose.orientation.w = angle
+        print(mba)
+        print(rtrans)
+        print(gtrans)
+        return mba  # my code has a degree yay
+
+
+    smach.StateMachine.add('findGates', executor(find_beacons),transitions={'done': 'pickGate', 'error': 'fail'},remapping={"result":"gatesFound"})
+
+    smach.StateMachine.add('chooseGate', executor(find_beacons),transitions={'done': 'pickGate', 'error': 'fail'},remapping={"gatesFound":"data"})
+    smach.StateMachine.add('navigate_to_start', moveTo(),transitions={'succeeded': 'find_end', 'preempted': 'fail', 'aborted': 'fail'})
+
+    def midpointEnd(data):
+        # Extract IDs of start and end beacons
+        items = []
+        listener = tf.TransformListener()
+        for i in data:
+            # im assuming this is the relevant thing?
+            listener.waitForTransform(
+                '/'+i.frame_id, '/map', rospy.Time(0), rospy.Duration(4.0))
+            (trans, rot) = listener.lookupTransform(
+                '/'+i.frame_id, '/map', rospy.Time(0))
+            dist = math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+            items.append((dist, i.best_guess, i.frame_id))
+        # sort to give us closest items.
+        items = sorted(items, key=lambda i: i[0],reverse=True)
+        firstRed = None
+        firstGreen = None
+        for i in items:
+            if firstRed is None and i[1].find("red"):
+                firstRed = i
+            if firstGreen is None and i[1].find("green"):
+                firstGreen = i
+        (rtrans, rot) = listener.lookupTransform(
+            firstRed[2], '/map', rospy.Time(0))
+        (gtrans, rot) = listener.lookupTransform(
+            firstGreen[2], '/map', rospy.Time(0))
+        midpoint = (-(rtrans[0]+gtrans[0])/2, -(rtrans[1]+gtrans[1])/2)
+        angle = math.atan2((rtrans[0]-gtrans[0]),
+                            (rtrans[1]-gtrans[1]))+math.pi/4
+        mba = PoseStamped()
+        mba.pose.position.x = midpoint[0]
+        mba.pose.position.y = midpoint[1]
+        mba.pose.position.z = 0.0
+        # fix quarternion
+        mba.pose.orientation.x = 0
+        mba.pose.orientation.y = 0
+        mba.pose.orientation.z = 0
+        mba.pose.orientation.w = angle
+        print(mba)
+        print(rtrans)
+        print(gtrans)
+        return mba  # my code has a degree yay
+    smach.StateMachine.add('find_end', executor(midpointEnd),transitions={'done': 'navigate_to_end', 'error': 'fail'},remapping={"result":"goal"})
+    smach.StateMachine.add('navigate_to_end', moveTo(),transitions={'succeeded': 'success', 'preempted': 'fail', 'aborted': 'fail'})
+
+if __name__ == '__main__':
+    rospy.init_node('smach_demonav_state_machine')
+    # start introspection server
+    # Create and start the introspection server
+    if rospy.get_param("machineDebug",False)==True:
+        sis = smach_ros.IntrospectionServer('server_name', machine, '/SM_ROOT')
+        sis.start()
+    # Execute the state machine
+    outcome = machine.execute()
+    # Wait for ctrl-c to stop the application
+    rospy.spin()
+    sis.stop()
+
+
+
+
+
+
